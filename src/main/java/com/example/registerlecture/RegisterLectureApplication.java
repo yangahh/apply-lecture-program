51269ec20703c4e25084ff3dc06ef8414b5d9058
yangahh <toaur6802@naver.com>
2024-12-24 19:51:17 +0900

    setting: springboot init setting

---
+++ src/main/java/com/example/registerlecture/RegisterLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.registerlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registerlecture/RegisterLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
